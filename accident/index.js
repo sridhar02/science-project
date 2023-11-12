857dc74407a46cc7e0fc89bb51acd0a53aac1e06
sridhar02 <kattasridhar02@gmail.com>
2023-11-12 11:45:05 +0530

    add accelerometer

--- accident/index.js
+++ accident/index.js
@@ -1,51 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the button element
-  let button = document.getElementById("acc");
-  // Get the elements we want to write to
-  let xElement = document.getElementById("x");
-  let yElement = document.getElementById("y");
-  let zElement = document.getElementById("z");
-  // Function to handle the button click
-  button.addEventListener("click", function () {
-    // Check if the accelerometer is supported by the browser
-    if (window.DeviceMotionEvent) {
-      // Ask the user for permission to access the accelerometer
-      if (
-        window.confirm(
-          "Allow access to the accelerometer? You can deny access at any time in your browser settings."
-        )
-      ) {
-        // Add event listener for device motion
-        window.addEventListener("devicemotion", accelerometerUpdate, true);
-        // Change the text of the button to show that access has been granted
-        button.textContent = "YES";
-        button.className = "yes";
+if (typeof DeviceMotionEvent.requestPermission === "function") {
+  // Request permission to access the accelerometer if available
+  DeviceMotionEvent.requestPermission()
+    .then((permissionState) => {
+      if (permissionState === "granted") {
+        // DeviceMotionEvent is supported and permission is granted
+        window.addEventListener("devicemotion", handleMotion);
       } else {
-        // If the user does not allow access, change the text of the button and remove the event listener
-        button.textContent = "NO";
-        button.className = "no";
-        window.removeEventListener("devicemotion", accelerometerUpdate, true);
+        // Permission denied, display an error message
+        showError("Permission to access the accelerometer is denied.");
       }
-    } else {
-      console.log("Accelerometer not supported on this browser/device");
-    }
-  });
-});
+    })
+    .catch((error) => {
+      // Handle errors
+      showError("Error requesting accelerometer permission: " + error.message);
+    });
+} else {
+  // DeviceMotionEvent is not supported in this browser
+  showError("Accelerometer API is not supported in this browser.");
+}
 
-// Function to handle the device motion event
-function accelerometerUpdate(event) {
-  // Get the acceleration values
-  let aX = event.accelerationIncludingGravity.x * 10;
-  let aY = event.accelerationIncludingGravity.y * 10;
-  let aZ = event.accelerationIncludingGravity.z * 10;
-  // Update the elements with the new values
-  xElement.value = aX;
-  yElement.value = aY;
-  zElement.value = aZ;
-  // If the Y value is negative, switch the X value's sign and subtract 180 to adjust for the rotation
-  if (aY < 0) {
-    aX = -aX - 180;
-  }
-  // Rotate the #block element based on the X value
-  document.querySelector("#block").style.transform = "rotate(" + aX + "deg)";
+function handleMotion(event) {
+  // Extract accelerometer values
+  const accelerationX = event.acceleration.x;
+  const accelerationY = event.acceleration.y;
+  const accelerationZ = event.acceleration.z;
+
+  // Update the UI with the values
+  document.getElementById("x-value").textContent = accelerationX.toFixed(2);
+  document.getElementById("y-value").textContent = accelerationY.toFixed(2);
+  document.getElementById("z-value").textContent = accelerationZ.toFixed(2);
+}
+
+function showError(message) {
+  // Display an error message in the UI
+  const errorElement = document.createElement("p");
+  errorElement.textContent = message;
+  errorElement.style.color = "red";
+  document.getElementById("acceleration-values").appendChild(errorElement);
 }
