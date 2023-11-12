6579d6578081403a6e6dcfda1de9d5fbdb1f86ea
sridhar02 <kattasridhar02@gmail.com>
2023-11-12 21:42:55 +0530

    Site

--- site/index.js
+++ site/index.js
@@ -27,7 +27,23 @@ function showCameraSection() {
 
 function startCamera() {
   navigator.mediaDevices
-    .getUserMedia({ video: true })
+    .getUserMedia({
+      video: {
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440,
+        },
+        facingMode: {
+          exact: "environment",
+        },
+      },
+    })
     .then(function (stream) {
       var video = document.getElementById("cameraStream");
       video.srcObject = stream;
