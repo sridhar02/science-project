781360639b5a1dc86d1720dac4b0176861a8440c
Ramana Venkata <vramana@users.noreply.github.com>
2023-11-18 00:18:14 +0530

    Prepare website

--- accident/index.js
+++ accident/index.js
@@ -1,159 +1,91 @@
-var xChart = echarts.init(document.getElementById("x-chart"));
-var yChart = echarts.init(document.getElementById("y-chart"));
-var zChart = echarts.init(document.getElementById("z-chart"));
+let chart = echarts.init(document.getElementById('main'));
 
-const clearButton = document.getElementById("clear");
-const timeOut = 100;
-
-if (window.DeviceMotionEvent == undefined) {
-  //No accelerometer is present. Use buttons.
-  console.log("No accelerometer present");
-} else {
-  window.addEventListener(
-    "devicemotion",
-    _.throttle(accelerometerUpdate, timeOut),
-    true
-  );
+// Initialize x-axis data up to 5 seconds
+let initialXAxisData = [];
+for (let i = 0; i <= 5000; i += 100) {
+    initialXAxisData.push(i);
 }
 
-function accelerometerUpdate(event) {
-  console.log({ x: event.acceleration.x, t: event.time });
-  let time = (event.time || 16) / 1000;
-
-  var aX = clampValue(event.acceleration.x) * time;
-  var aY = clampValue(event.acceleration.y) * time;
-  var aZ = clampValue(event.acceleration.z) * time;
-
-  updateGraphs({ x: aX, y: aY, z: aZ });
-  document.getElementById("interval").innerHTML = event.interval;
-}
-
-// Mock accelerometer data
-function generateMockAccelerometerData() {
-  const maxAcceleration = 10; // You can adjust this value as needed
-  const mockData = {
-    acceleration: {
-      x: Math.random() * maxAcceleration,
-      y: Math.random() * maxAcceleration,
-      z: Math.random() * maxAcceleration,
+let option = {
+    xAxis: {
+        type: 'category',
+        data: initialXAxisData
     },
-  };
-
-  return mockData;
-}
-
-function clampValue(value) {
-  if (value < 0.2 && value > -0.2) {
-    return 0;
-  }
-  return value;
-}
-let x = 0,
-  y = 0,
-  z = 0;
-
-function updateGraphs(data) {
-  time = time + timeOut;
-  xOption.xAxis.data.push(time);
-  yOption.xAxis.data.push(time);
-  zOption.xAxis.data.push(time);
-
-  x += data.x;
-  y += data.y;
-  z += data.z;
-
-  xOption.series[0].data.push(x);
-  xOption && chartX.setOption(xOption);
-
-  yOption.series[0].data.push(y);
-  yOption && chartY.setOption(yOption);
-
-  zOption.series[0].data.push(z);
-  zOption && chartZ.setOption(zOption);
-}
-
-// setInterval(() => {
-//   const mockData = generateMockAccelerometerData();
-//   accelerometerUpdate({ acceleration: mockData.acceleration });
-//   updateGraphs(mockData.acceleration);
-// }, 1000);
-
-var xChart = document.getElementById("x-chart");
-var chartX = echarts.init(xChart);
-
-var yChart = document.getElementById("y-chart");
-var chartY = echarts.init(yChart);
-
-var zChart = document.getElementById("z-chart");
-var chartZ = echarts.init(zChart);
-
-var xOption, yOption, zOption;
-let time = timeOut;
-
-xOption = {
-  xAxis: {
-    type: "category",
-    data: [],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [],
-      type: "line",
+    yAxis: {
+        type: 'value',
+        min: 0,
+        max: 50 // Maximum value for Y-axis
     },
-  ],
+    series: [{
+        data: [],
+        type: 'line'
+    }]
 };
 
-yOption = {
-  xAxis: {
-    type: "category",
-    data: [],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [],
-      type: "line",
-    },
-  ],
-};
+let timeCounter, speed, isStopping, isUpdating, timeoutId;
 
-zOption = {
-  xAxis: {
-    type: "category",
-    data: [],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [],
-      type: "line",
-    },
-  ],
-};
+function resetData() {
+    timeCounter = 0; // Reset time to 0
+    speed = 0;
+    isStopping = false;
+    isUpdating = false;
+    option.xAxis.data = initialXAxisData.slice(0);
+    option.series[0].data = [];
+    chart.setOption(option, true);
+}
 
-xOption && chartX.setOption(xOption);
-yOption && chartY.setOption(yOption);
-zOption && chartZ.setOption(zOption);
+resetData();
 
-clearButton.addEventListener("click", function () {
-  time = 0;
-  xOption.xAxis.data = [];
-  yOption.xAxis.data = [];
-  zOption.xAxis.data = [];
-
-  xOption.series[0].data = [];
-  xOption && chartX.setOption(xOption);
-
-  yOption.series[0].data = [];
-  yOption && chartY.setOption(yOption);
-
-  zOption.series[0].data = [];
-  zOption && chartZ.setOption(zOption);
+document.getElementById('startButton').addEventListener('click', function() {
+    resetData();
+    isUpdating = true;
+    updateChart();
 });
+
+document.getElementById('stopButton').addEventListener('click', function() {
+    isStopping = true;
+});
+
+document.getElementById('accidentButton').addEventListener('click', function() {
+    speed = 0;
+    isUpdating = false; // Stop updating the chart
+    if (timeoutId) {
+        clearTimeout(timeoutId); // Clear the timeout when an accident occurs
+    }
+    if (option.series[0].data.length > 0) {
+        option.series[0].data[option.series[0].data.length - 1] = 0;
+        chart.setOption(option);
+    }
+});
+
+function updateChart() {
+    if (!isUpdating) return;
+
+    timeoutId = setTimeout(function() {
+        timeCounter += 100;
+
+        if (isStopping) {
+            if (speed > 5) {
+                speed -= Math.random() * 2 + 1;
+            } else {
+                speed = 0;
+                isUpdating = false;
+            }
+        } else {
+            // Increase speed gradually until it reaches 40, then fluctuate between 40 and 60
+            if (speed < 40) {
+                speed += Math.random() * 3 + 1;
+            } else {
+                speed = 40 + Math.random() * 5;
+            }
+        }
+
+        option.xAxis.data.push(timeCounter);
+        option.series[0].data.push(speed);
+        chart.setOption(option);
+
+        if (isUpdating) {
+            updateChart();
+        }
+    }, 100);
+}
